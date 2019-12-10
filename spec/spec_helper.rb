1a4fa56b9b467d6f0735d78c45a131dcf086d05c
Pedro Pereira <pedro.pereira@shore.com>
2019-12-10 18:33:31 +0100

    Add database-cleaner

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 ENV['RAILS_ENV'] ||= 'test'
+
 require_relative '../config/environment'
+require 'database_cleaner'
+
+RSpec.configure do |config|
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+end
