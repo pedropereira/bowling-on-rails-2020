5b4a7205b4e38e314d4a4eea7b5d061fc762af8d
Pedro Pereira <pedro.pereira@shore.com>
2019-12-11 09:06:05 +0100

    Add start game service object

---
+++ app/services/start_game.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class StartGame
+  def call
+    game = game_repository.create
+    frame_repository.create(game_id: game.id)
+
+    game
+  end
+
+  private
+
+  def frame_repository
+    @frame_repository ||= FrameRepository.new
+  end
+
+  def game_repository
+    @game_repository ||= GameRepository.new
+  end
+end
