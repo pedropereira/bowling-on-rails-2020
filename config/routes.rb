7237db19c28251c749f134c9e2d4f42d55b92479
Pedro Pereira <pedro.pereira@shore.com>
2020-04-09 00:34:05 +0200

    Add score route

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      post '/games/start', to: 'start#call'
+      get 'games/:id', to: 'score#call'
 
-      post '/games/:id/roll', to: 'roll#call'
+      post 'games/start', to: 'start#call'
+
+      post 'games/:id/roll', to: 'roll#call'
     end
   end
 end
