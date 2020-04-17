940158c7bdb9d6a5597ab0df8a614619fd23ff82
Pedro Pereira <pedro.pereira@shore.com>
2020-04-17 09:38:37 +0200

    Add specs for serializers/score and use_cases/get_score

---
+++ spec/lib/serializers/score_spec.rb
@@ -0,0 +1,185 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Serializers::Score do
+  describe '#call' do
+    it 'returns the serialized payload of a game' do
+      model = build_model
+      game = Entities::Game.new(model)
+      serializer = described_class.new(game)
+
+      result = serializer.call
+
+      expect(result).to eq(build_payload(game))
+    end
+  end
+
+  def build_model
+    FactoryBot.create(
+      :game,
+      frames: [
+        FactoryBot.create(
+          :frame,
+          rolls: [
+            FactoryBot.create(
+              :roll,
+              pins: 10
+            )
+          ]
+        )
+      ]
+    )
+  end
+
+  def build_payload(game)
+    game_id = game.id
+    frame_id = game.frames.first.id
+    roll_id = game.frames.first.rolls.first.id
+
+    {
+      data: {
+        id: game_id,
+        type: 'game',
+        attributes: {
+          state: 'ongoing',
+          score: 10
+        },
+        relationships: {
+          frames: {
+            data: [
+              {
+                id: frame_id,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: [
+                      {
+                        id: roll_id,
+                        type: 'roll',
+                        attributes: {
+                          pins: 10
+                        }
+                      }
+                    ]
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+  end
+end
---
+++ spec/lib/use_cases/get_score_spec.rb
@@ -0,0 +1,184 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UseCases::GetScore do
+  describe '#call' do
+    it 'returns game score payload' do
+      model = build_model
+      game = Entities::Game.new(model)
+
+      result = described_class.new.call(game.id)
+
+      expect(result).to eq(build_payload(game))
+    end
+  end
+
+  def build_model
+    FactoryBot.create(
+      :game,
+      frames: [
+        FactoryBot.create(
+          :frame,
+          rolls: [
+            FactoryBot.create(
+              :roll,
+              pins: 10
+            )
+          ]
+        )
+      ]
+    )
+  end
+
+  def build_payload(game)
+    game_id = game.id
+    frame_id = game.frames.first.id
+    roll_id = game.frames.first.rolls.first.id
+
+    {
+      data: {
+        id: game_id,
+        type: 'game',
+        attributes: {
+          state: 'ongoing',
+          score: 10
+        },
+        relationships: {
+          frames: {
+            data: [
+              {
+                id: frame_id,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: [
+                      {
+                        id: roll_id,
+                        type: 'roll',
+                        attributes: {
+                          pins: 10
+                        }
+                      }
+                    ]
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              },
+              {
+                id: nil,
+                type: 'frame',
+                attributes: {
+                  score: 0
+                },
+                relationships: {
+                  rolls: {
+                    data: []
+                  }
+                }
+              }
+            ]
+          }
+        }
+      }
+    }
+  end
+end
